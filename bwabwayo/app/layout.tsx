41c45a7d190c5b019e6c5c248706deb40c631a3a
Jodndud <doriconi@gmail.com>
2025-07-24 12:38:11 +0900

    start!

---
+++ bwabwayo/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "봐봐요",
+  description: "화상중고거래 플랫폼, 봐봐요",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <nav>헤더입니다</nav>
+        {children}
+        <footer>푸터입니다</footer>
+      </body>
+    </html>
+  );
+}
