d1c36ba921b5801a939767006db412d80a87571f
Jodndud <doriconi@gmail.com>
2025-07-25 13:58:51 +0900

    250725 13:35

--- bwabwayo/app/chat/[productId]/[sellerId]/page.tsx
+++ bwabwayo/app/chat/[productId]/[sellerId]/page.tsx
@@ -4,11 +4,11 @@ import ClientChat from './ClientChat'
 
 type Props = {
   params: {
-    id: string
-    seller_id: string
+    productId: string,
+    sellerId: string
   }
 }
 
 export default function ChatPage({ params }: Props) {
-  return <ClientChat productId={params.id} sellerId={params.seller_id} />
+  return <ClientChat productId={params.productId} sellerId={params.sellerId} />
 }
