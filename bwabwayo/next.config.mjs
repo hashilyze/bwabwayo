2a66dc99def7db8c5ec7ed3407a9e975962a824d
jodndud <doriconi@gmail.com>
2025-08-07 20:57:50 +0900

    URL환경변수 설정

---
+++ bwabwayo/next.config.mjs
@@ -0,0 +1,29 @@
+const nextConfig = {
+  basePath: '/fe',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bwabwayo-general-bucket.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://localhost:8081/:path*', // dev 백엔드 주소
+      },
+    ];
+  },
+};
+
+export default nextConfig;
