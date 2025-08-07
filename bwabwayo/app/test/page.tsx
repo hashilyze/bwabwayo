e3616e1930f215f69786edf26d3a989a75b7dc9d
DESKTOP-DLOPF19\SSAFY <yoonsu0325@gmail.com>
2025-08-07 16:25:05 +0900

    feat: 채팅방 시스템 메세지 모달

--- bwabwayo/app/test/page.tsx
+++ bwabwayo/app/test/page.tsx
@@ -2,26 +2,50 @@ import AllModals from "@/components/chat/modals/AllModals"
 
 export default function TestPage() {
     return (
-        <div className="grid grid-cols-4 gap-10">
+        <div className="grid grid-cols-2 gap-10">
             <div className="flex flex-col gap-4 justify-center items-center mb-10">
                 <h1>화상 예약 알림</h1>
-                <AllModals message={{ content: '화상 예약 알림', createdAt: '2025-08-06T00:00:00' }} type="video" />
+                <AllModals message={{ content: '화상 예약 알림', createdAt: '2025-08-06T00:00:00' }} type="RESERVE_VIDEOCALL" />
+            </div>
+            <div className="flex flex-col gap-4 justify-center items-center mb-10">
+                <h1>화상 예약 취소</h1>
+                <AllModals message={{ content: '화상 예약 취소', createdAt: '2025-08-06T00:00:00' }} type="CANCEL_VIDEOCALL" />
+            </div>
+            <div className="flex flex-col gap-4 justify-center items-center mb-10">
+                <h1>화상 거래 시작</h1>
+                <AllModals message={{ content: '화상 거래 시작', createdAt: '2025-08-06T00:00:00' }} type="START_VIDEOCALL" />
             </div>
             <div className="flex flex-col gap-4 justify-center items-center mb-10">
                 <h1>거래 시작 - 최종 금액 확인</h1>
-                <AllModals message={{ content: '최종 금액 확인', createdAt: '2025-08-06T00:00:00' }} type="start" />
+                <AllModals message={{ content: '최종 금액 확인', createdAt: '2025-08-06T00:00:00' }} type="START_TRADE" />
             </div>
             <div className="flex flex-col gap-4 justify-center items-center mb-10">
-                <h1>배송지와 계좌 공지</h1>
-                <AllModals message={{ content: '최종 금액이 결정되었어요!', createdAt: '2025-08-06T00:00:00' }} type="lotation" />
+                <h1>결제요청</h1>
+                <AllModals message={{ content: '결제 요청', createdAt: '2025-08-06T00:00:00' }} type="REUEST_DEPOSIT" />
             </div>
             <div className="flex flex-col gap-4 justify-center items-center mb-10">
-                <h1>입금 확인</h1>
-                <AllModals message={{ content: '입금이 확인되었어요!', createdAt: '2025-08-06T00:00:00' }} type="check" />
+                <h1>배송지 공지</h1>
+                <AllModals message={{ content: '배송지가 입력되었어요', createdAt: '2025-08-06T00:00:00' }} type="INPUT_TRACKING_NUMBER" />
             </div>
             <div className="flex flex-col gap-4 justify-center items-center mb-10">
-                <h1>배송 조회</h1>
-                <AllModals message={{ content: '배송 조회', createdAt: '2025-08-06T00:00:00' }} type="delivery" />
+                <h1>채팅방 생성</h1>
+                <AllModals message={{ content: '채팅방이 생성되었어요!', createdAt: '2025-08-06T00:00:00' }} type="CREATE_ROOM" />
+            </div>
+            <div className="flex flex-col gap-4 justify-center items-center mb-10">
+                <h1>배송지 입력</h1>
+                <AllModals message={{ content: '배송지 입력', createdAt: '2025-08-06T00:00:00' }} type="INPUT_DELIVERY_ADDRESS" />
+            </div>
+            <div className="flex flex-col gap-4 justify-center items-center mb-10">
+                <h1>배송 시작</h1>
+                <AllModals message={{ content: '배송 시작', createdAt: '2025-08-06T00:00:00' }} type="START_DELIVERY" />
+            </div>
+            <div className="flex flex-col gap-4 justify-center items-center mb-10">
+                <h1>구매 확정 요청</h1>
+                <AllModals message={{ content: '구매 확정 요청', createdAt: '2025-08-06T00:00:00' }} type="CONFIRM_PURCHASE" />
+            </div>
+            <div className="flex flex-col gap-4 justify-center items-center mb-10">
+                <h1>구매 확정</h1>
+                <AllModals message={{ content: '구매 확정', createdAt: '2025-08-06T00:00:00' }} type="END_TRADE" />
             </div>
         </div>
     )
