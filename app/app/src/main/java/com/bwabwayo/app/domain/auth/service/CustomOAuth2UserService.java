25996f454f03072df804d1013b3aafef640e4732
이재원 <dlwodnjs9354@gmail.com>
2025-08-08 14:15:57 +0900

    hotfix: 소셜 로그인 수정 및 로그 추가

--- app/app/src/main/java/com/bwabwayo/app/domain/auth/service/CustomOAuth2UserService.java
+++ app/app/src/main/java/com/bwabwayo/app/domain/auth/service/CustomOAuth2UserService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
@@ -35,12 +36,17 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         if (userRequest.getClientRegistration().getRegistrationId().equals("kakao")) {
             log.info("카카오 정보 가져오기");
             oAuth2UserInfo = new KakaoUserInfo(oAuth2User.getAttributes());
+        } else {
+            throw new OAuth2AuthenticationException(
+                    new OAuth2Error("invalid_provider"), // 에러 코드
+                    "지원하지 않는 OAuth2 Provider: " + userRequest.getClientRegistration().getRegistrationId()
+            );
         }
 
         User userEntity =
                 userService.findById(oAuth2UserInfo.getProviderId());
         OAuth2UserRequest user;
-        log.info(userEntity.toString());
+        log.info("userEntity : {}", userEntity);
         //null이면 새가입자, isActive가 false면 재가입자
         if (userEntity == null || !userEntity.isActive()) {
             log.info("새가입자 혹은 재가입자");
