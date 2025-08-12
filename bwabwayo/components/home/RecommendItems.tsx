0ee5ca8dda1467ee393f8d659a0705612b74e35b
장종원 <wkdwhd7598@naver.com>
2025-08-12 09:44:10 +0900

    fix: 메인 페이지 신발 -> 조던

--- bwabwayo/components/home/RecommendItems.tsx
+++ bwabwayo/components/home/RecommendItems.tsx
@@ -24,7 +24,7 @@ export default function RecommendItems() {
         },
         {
             id: 5,
-            name: '신발',
+            name: '조던던',
             image: 'image/rec-category/jordan.png'
         },
         {
