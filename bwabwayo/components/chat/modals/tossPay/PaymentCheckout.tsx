c99a0e158646444358834abb3d7edcf16f22fdcd
Jodndud <doriconi@gmail.com>
2025-08-13 17:59:27 +0900

    goHome

--- bwabwayo/components/chat/modals/tossPay/PaymentCheckout.tsx
+++ bwabwayo/components/chat/modals/tossPay/PaymentCheckout.tsx
@@ -92,7 +92,7 @@ export function PaymentCheckoutPage({ onClose, amount, orderName, roomId, produc
           amount: paymentAmount,
           orderId: generateRandomString(), // 고유 주문번호
           orderName: orderName,
-          successUrl: window.location.origin + `${process.env.NEXT_PUBLIC_PUBLIC_URL}/payment/success?roomId=${roomId}&productId=${productId}`, // 결제 요청이 성공하면 리다이렉트되는 URL
+          successUrl: `${process.env.NEXT_PUBLIC_PUBLIC_URL}/chat/${roomId}`, // 결제 요청이 성공하면 리다이렉트되는 URL
           failUrl: window.location.origin + "/fail", // 결제 요청이 실패하면 리다이렉트되는 URL
           customerEmail: "customer123@gmail.com",
           customerName: userData?.nickname || "사용자",
